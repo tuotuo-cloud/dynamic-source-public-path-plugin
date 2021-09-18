ddf14cc42b3c2016e8c2729f63a426cbd67896cc
zsq <zengsiqing@meituan.com>
2021-09-18 10:47:30 +0800

    修改html-webpack-plugin的引入逻辑，避免html-webpack-plugin钩子不执行

--- index.js
+++ index.js
@@ -1,85 +1,71 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-class DynamicLoadHtmlWebpackPlugin {
-  constructor(options = {}) {
-    const {publicPathTemplate} = options;
+class DynamicSourcePublicPathPlugin {
+  constructor(options) {
+    const { publicPathTemplate } = options;
     this.callbackName = 'asyncAppendNode';
     this.cdnVariableName = 'window.publicPath';
     this.publicPathTemplate = publicPathTemplate;
+
+    this.extractHtmlWebpackPluginModule = (compiler) => {
+      const htmlWebpackPlugin = (compiler.options.plugins || []).find(plugin => {
+        return plugin.constructor.name === 'HtmlWebpackPlugin';
+      });
+      if (!htmlWebpackPlugin) {
+        return null;
+      }
+      const HtmlWebpackPlugin = htmlWebpackPlugin.constructor;
+      if (!HtmlWebpackPlugin || !('getHooks' in HtmlWebpackPlugin)) {
+        return null;
+      }
+      return HtmlWebpackPlugin;
+    };
   }
 
   rewriteData(node, nodeList, fnName, publicPath) {
-    if (node === "link") {
-      const fileNames = nodeList.map(item => item.attributes.href.split("/").pop());
-      const scriptInnerHtml = fileNames
-        .map(item => `${fnName}('${node}','${item}');`)
-        .join("\n");
-      return [{tagName: "script", voidTag: false, innerHTML: scriptInnerHtml}];
+    if (node === 'link') {
+      const fileNames = nodeList.map((item) => item?.attributes?.href?.split('/').pop());
+      const scriptInnerHtml = fileNames.map((item) => `${fnName}('${node}','${item}');`).join('');
+      return { tagName: 'script', voidTag: false, innerHTML: scriptInnerHtml };
     }
-    if(node === "script"){
+    if (node === 'script') {
       const inlineScript = [];
       const srcScript = [];
-      nodeList.forEach(item => {
+      nodeList.forEach((item) => {
         if (item.innerHTML) {
-          if (typeof publicPath === "string" && this.cdnVariableName) {
-            const html = item.innerHTML;
-            // TODO 内联script的处理可能存在问题，replace使用正则匹配可能更好
-            const newHtml = html.replace(
-              `="${publicPath}"`,
-              `=${this.cdnVariableName}`
-            );
-            item.innerHTML = newHtml;
+          if (typeof publicPath === 'string') {
+            item.innerHTML = item.innerHTML.replace(`="${publicPath}"`, `=${this.cdnVariableName}`);
           }
           inlineScript.push(item);
         } else {
-          srcScript.push(item.attributes.src.split("/").pop());
+          srcScript.push(item?.attributes?.src?.split('/').pop());
         }
       });
-      const scriptInnerHtml = srcScript
-        .map(item => `${fnName}('${node}','${item}');`)
-        .join("\n");
-      return [
-        ...inlineScript,
-        {tagName: "script", closeTag: true, innerHTML: scriptInnerHtml}
-      ];
+      const scriptInnerHtml = srcScript.map((item) => `${fnName}('${node}','${item}');`).join('');
+      return [...inlineScript, { tagName: 'script', closeTag: true, innerHTML: scriptInnerHtml }];
     }
   }
 
   apply(compiler) {
-    compiler.hooks.compilation.tap(
-      "DynamicLoadHtmlWebpackPlugin",
-      compilation => {
-        HtmlWebpackPlugin.getHooks(
-          compilation
-        ).beforeAssetTagGeneration.tapAsync(
-          "DynamicLoadHtmlWebpackPlugin",
-          (data, cb) => {
-            this.publicPath = data.assets.publicPath;
-            cb(null, data);
-          }
-        );
-        HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
-          "DynamicLoadHtmlWebpackPlugin",
-          (data, cb) => {
-            if (data.headTags.length) {
-              const newStyleData = this.rewriteData(
-                "link",
-                data.headTags,
-                this.callbackName
-              );
-              data.headTags = newStyleData;
-            }
-            if (data.bodyTags.length) {
-              const newScriptData = this.rewriteData(
-                "script",
-                data.bodyTags,
-                this.callbackName,
-                this.publicPath
-              );
-              data.bodyTags = newScriptData;
-            }
+    compiler.hooks.compilation.tap('DynamicSourcePublicPathPlugin', (compilation) => {
+      const HtmlWebpackPlugin = this.extractHtmlWebpackPluginModule(compiler);
+      if (!HtmlWebpackPlugin) {
+        throw new Error('HtmlWebpackInjectPreload needs to be used with html-webpack-plugin 4 or 5');
+      }
+      const hooks = HtmlWebpackPlugin.getHooks(compilation);
+      hooks.beforeAssetTagGeneration.tapAsync('DynamicSourcePublicPathPlugin', (data, cb) => {
+        this.publicPath = data.assets.publicPath;
+        cb(null, data);
+      });
+      hooks.afterTemplateExecution.tapAsync('DynamicSourcePublicPathPlugin', (data, cb) => {
+        if (data.headTags.length) {
+          const notLinkHeadTags = data.headTags.filter(item => item.tagName !== 'link');
+          const linkHeadTags = this.rewriteData('link', data.headTags.filter(item => item.tagName === 'link'), this.callbackName);
+          data.headTags = [].concat(notLinkHeadTags, linkHeadTags);
+        }
+        if (data.bodyTags.length) {
+          data.bodyTags = this.rewriteData('script', data.bodyTags, this.callbackName, this.publicPath);
+        }
 
-            const topScript = `
+        const topScript = `
             <script>
               var host = window.location.host;
               var publicPathTemplate = ${this.publicPathTemplate.toString()}
@@ -97,15 +83,13 @@ class DynamicLoadHtmlWebpackPlugin {
                 }
               }
             </script>
-            `
-            data.html = data.html.replace('<!--SetGlobalPublicPath inset script-->', topScript)
+            `;
+        data.html = data.html.replace('<!--SetGlobalPublicPath inset script-->', topScript);
 
-            cb(null, data);
-          }
-        );
-      }
-    );
+        cb(null, data);
+      });
+    });
   }
 }
 
-module.exports = DynamicLoadHtmlWebpackPlugin;
+module.exports = DynamicSourcePublicPathPlugin;
